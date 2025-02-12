cbdaaa88ceec29dc786c34353845b50afe0316f6
Taheera Ahmed <taheera.ahmed@nina.no>
2025-02-12 21:16:05 +0100

    initial commit

---
+++ src/main.py
@@ -0,0 +1,21 @@
+import logging
+import pathlib
+
+import environ
+
+env = environ.Env()
+BASE_DIR = pathlib.Path(__file__).parent.parent
+environ.Env.read_env(str(BASE_DIR / ".env"))
+
+DEBUG = env.bool("DEBUG", default=False)
+
+logging.basicConfig(level=(logging.DEBUG if DEBUG else logging.INFO))
+
+
+def start() -> None:
+    logging.debug("only printed in DEBUG")
+    print("hello world!")
+
+
+if __name__ == "__main__":
+    start()
